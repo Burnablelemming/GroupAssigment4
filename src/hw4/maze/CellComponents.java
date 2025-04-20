cad37a8def6c72bf3b1a2837406d254b169c77a9
Lane Flint <lane.blair.flint@gmail.com>
2025-04-19 22:51:03 -0500

    Added Javadoc to CellComponents.java

--- src/hw4/maze/CellComponents.java
+++ src/hw4/maze/CellComponents.java
@@ -1,5 +1,12 @@
 package hw4.maze;
 
+/**
+ * Represents the possible components on each side of a cell
+ * 
+ * WALL 		a solid boundary that cannot be passed through
+ * APERTURE 	an open space that allows movement
+ * EXIT 		the designated exit used to win or escape the grid
+ */
 public enum CellComponents {
 	WALL,
 	APERTURE,
