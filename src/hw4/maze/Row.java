9dfb55276d92e52511b296362c3f3fe133568dbe
Lane Flint <lane.blair.flint@gmail.com>
2025-04-19 23:15:16 -0500

    Added Javadocs in Row.java class

--- src/hw4/maze/Row.java
+++ src/hw4/maze/Row.java
@@ -2,19 +2,37 @@ package hw4.maze;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a single row in the maze.
+ * A row is composed of a list of Cell objects arranged horizontally within the grid.
+ */
 public class Row {
 	
 	private ArrayList<Cell> cells;
 
+	/**
+	 * Constructs a Row with the specified list of cells.
+	 *
+	 * @param cells 		the list of Cell objects in the row
+	 */
 	public Row(ArrayList<Cell> cells) {
 	    this.cells = cells;
 	}
 
-	
+	/**
+	 * Returns the list of cells in the row.
+	 *
+	 * @return the list of Cell objects
+	 */
 	public ArrayList<Cell> getCells() {
 		return cells;
 	}
 
+	/**
+	 * Replaces the current list of cells with the specified list.
+	 *
+	 * @param cells 		the new list of Cell objects
+	 */
 	public void setCells(ArrayList<Cell> cells) {
 		this.cells = cells;
 	}
