2898630bff119534c7a7ff8a01aefdff1b39e7e2
Lane Flint <lane.blair.flint@gmail.com>
2025-04-19 23:28:02 -0500

    Added Javadocs for Player.java class

--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -3,28 +3,59 @@ package hw4.player;
 import hw4.maze.Cell;
 import hw4.maze.Row;
 
+/**
+ * Represents the player in the maze.
+ * Tracks the player's current location by referencing both the current Row and the current Cell within that row.
+ */
+
 public class Player {
 	
 	private Row currentRow;
 	private Cell currentCell;
 	
+	/**
+	 * Constructs a Player with the specified current row and cell.
+	 *
+	 * @param currentRow  		the row in which the player is located
+	 * @param currentCell 		the cell within the current row where the player is located
+	 */
 	public Player(Row currentRow, Cell currentCell) {
 		this.currentRow = currentRow;
 		this.currentCell = currentCell;
 	}
 
+	/**
+	 * Returns the player's current row.
+	 *
+	 * @return the current Row object
+	 */
 	public Row getCurrentRow() {
 		return currentRow;
 	}
 
+	/**
+	 * Sets the player's current row.
+	 *
+	 * @param currentRow 		the new Row the player is in
+	 */
 	public void setCurrentRow(Row currentRow) {
 		this.currentRow = currentRow;
 	}
 
+	/**
+	 * Returns the player's current cell within the current row.
+	 *
+	 * @return the current Cell object
+	 */
 	public Cell getCurrentCell() {
 		return currentCell;
 	}
 
+	/**
+	 * Sets the player's current cell.
+	 *
+	 * @param currentCell 		the new Cell the player is in.
+	 */
 	public void setCurrentCell(Cell currentCell) {
 		this.currentCell = currentCell;
 	}
